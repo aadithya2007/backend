408cb3141fbfceee87919a0a7466c4572cff5c13
aadithya2007 <aadithya2807@gmail.com>
2026-04-28 14:47:26 +0530

    frontend-backend connect

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
-const cors = require("cors");
+
 const app = express();
 app.use(express.json());
 app.use(
