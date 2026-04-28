1c3504407c191a4656a30014ba7a57e5f68de6c0
aadithya2007 <aadithya2807@gmail.com>
2026-04-28 14:39:57 +0530

    frontend-backend connect

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
-
+const cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://vegimart12.netlify.app",
+    ],
+    credentials: true,
+  })
+);
+
 
 const userRoutes = require("./Routers/UserRoutes");
 const productRoutes = require("./Routers/ProductRoutes");
